cc6d787b69eb5325a25c870a8e84bc3b8b5c8bdb
edgovigv-boop <edgovi.gv@gmail.com>
2026-06-11 11:20:08 -0600

    Fix Go Basket Studio score and name persistence

--- go-basket-studio/sw.js
+++ go-basket-studio/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'go-basket-studio-meta-clean-v1';
+const CACHE_NAME = 'go-basket-studio-stable-state-v2-v1';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
